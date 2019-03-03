b19b2803737b4c9b2725cf33ac03438591001bfb
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-03-03 10:14:21 +0100

    inheritance and endpoints of UserController
    - junit test use the TestRestTemplate and not MockMvc
    - formattet google style with 150 characters as right margin

--- src/main/java/com/gitlab/jactor/persistence/controller/UserController.java
+++ src/main/java/com/gitlab/jactor/persistence/controller/UserController.java
@@ -2,6 +2,7 @@ package com.gitlab.jactor.persistence.controller;
 
 import com.gitlab.jactor.persistence.dto.UserDto;
 import com.gitlab.jactor.persistence.service.UserService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,50 +10,56 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(value = "/user", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class UserController extends AbstractController {
+public class UserController {
 
-    private final UserService userServicey;
+  private final UserService userServicey;
 
-    @Autowired
-    public UserController(UserService userServicey) {
-        this.userServicey = userServicey;
+  @Autowired
+  public UserController(UserService userServicey) {
+    this.userServicey = userServicey;
+  }
+
+  @GetMapping("/name/{username}")
+  public ResponseEntity<UserDto> find(@PathVariable("username") String username) {
+    return userServicey.find(username)
+        .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
+        .orElseGet(() -> ResponseEntity.noContent().build());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<UserDto> get(@PathVariable("id") Long id) {
+    return userServicey.find(id)
+        .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+  }
+
+  @PostMapping()
+  public ResponseEntity<UserDto> post(@RequestBody UserDto userDto) {
+    UserDto saved = userServicey.saveOrUpdate(userDto);
+
+    return new ResponseEntity<>(saved, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{userId}")
+  public ResponseEntity<UserDto> put(@RequestBody UserDto userDto, @PathVariable Long userId) {
+    if (userDto == null || userDto.getId() == null || !userDto.getId().equals(userId)) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @GetMapping("/find/{username}")
-    public ResponseEntity<UserDto> find(@PathVariable("username") String username) {
-        return userServicey.find(username)
-                .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
-                .orElseGet(() -> ResponseEntity.noContent().build());
-    }
+    UserDto saved = userServicey.saveOrUpdate(userDto);
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<UserDto> get(@PathVariable("id") Long id) {
-        return userServicey.find(id)
-                .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
-    }
+    return new ResponseEntity<>(saved, HttpStatus.ACCEPTED);
+  }
 
-    @PostMapping("/persist")
-    public ResponseEntity<UserDto> save(@RequestBody UserDto userDto) {
-        UserDto saved = userServicey.saveOrUpdate(userDto);
-
-        if (userDto.getId() == null) {
-            return aCreatedResponseEntity(saved, "/user/get/" + saved.getId());
-        }
-
-        return new ResponseEntity<>(saved, HttpStatus.OK);
-    }
-
-    @GetMapping("/all/usernames")
-    public ResponseEntity<List<String>> findAllUsernames() {
-        return new ResponseEntity<>(userServicey.findUsernamesOnActiveUsers(), HttpStatus.OK);
-    }
+  @GetMapping("/active/usernames")
+  public ResponseEntity<List<String>> findAllUsernames() {
+    return new ResponseEntity<>(userServicey.findUsernamesOnActiveUsers(), HttpStatus.OK);
+  }
 }
--- src/test/java/com/gitlab/jactor/persistence/controller/UserControllerTest.java
+++ src/test/java/com/gitlab/jactor/persistence/controller/UserControllerTest.java
@@ -1,113 +1,164 @@
 package com.gitlab.jactor.persistence.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gitlab.jactor.persistence.dto.UserDto;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.gitlab.jactor.persistence.JactorPersistence;
+import com.gitlab.jactor.persistence.dto.UserDto;
 import com.gitlab.jactor.persistence.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {JactorPersistence.class})
+@SpringBootTest(classes = {JactorPersistence.class}, webEnvironment = WebEnvironment.RANDOM_PORT)
 @DisplayName("A UserController")
 class UserControllerTest {
 
-    private MockMvc mockMvc;
-    private @MockBean UserService userServiceMock;
-    private @Autowired ObjectMapper objectMapper;
+  @LocalServerPort
+  private int port;
+  @Value("${server.servlet.context-path}")
+  private String contextPath;
 
-    @BeforeEach void mockMvc() {
-        mockMvc = standaloneSetup(new UserController(userServiceMock)).build();
-    }
+  @MockBean
+  private UserService userServiceMock;
+  @Autowired
+  private TestRestTemplate testRestTemplate;
 
-    @DisplayName("should not find a user by username")
-    @Test void shouldNotFindByUsername() throws Exception {
-        when(userServiceMock.find("me")).thenReturn(Optional.empty());
+  @Test
+  @DisplayName("should build full path")
+  void shouldBuildFullPath() {
+    assertThat(buildFullPath("/somewhere")).isEqualTo("http://localhost:" + port + "/jactor-persistence-orm/somewhere");
+  }
 
-        mockMvc.perform(get("/user/find/me")).andExpect(status().isNoContent());
-    }
+  @Test
+  @DisplayName("should not find a user by username")
+  void shouldNotFindByUsername() {
+    when(userServiceMock.find("me")).thenReturn(Optional.empty());
 
-    @DisplayName("should find a user by username")
-    @Test void shouldFindByUsername() throws Exception {
-        when(userServiceMock.find("me")).thenReturn(Optional.of(new UserDto()));
+    var userRespnse = testRestTemplate.getForEntity(buildFullPath("/user/name/me"), UserDto.class);
 
-        mockMvc.perform(get("/user/find/me")).andExpect(status().isOk());
-    }
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.NO_CONTENT),
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getBody).as("user").isNull()
+    );
+  }
 
-    @DisplayName("should not find a user by id")
-    @Test void shouldNotFindById() throws Exception {
-        when(userServiceMock.find(1L)).thenReturn(Optional.empty());
+  @Test
+  @DisplayName("should find a user by username")
+  void shouldFindByUsername() {
+    when(userServiceMock.find("me")).thenReturn(Optional.of(new UserDto()));
 
-        mockMvc.perform(get("/user/get/1")).andExpect(status().isNoContent());
-    }
+    var userRespnse = testRestTemplate.getForEntity(buildFullPath("/user/name/me"), UserDto.class);
 
-    @DisplayName("should find a user by id")
-    @Test void shouldFindById() throws Exception {
-        when(userServiceMock.find(1L)).thenReturn(Optional.of(new UserDto()));
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.OK),
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getBody).as("user").isNotNull()
+    );
+  }
 
-        mockMvc.perform(get("/user/get/1")).andExpect(status().isOk());
-    }
+  @Test
+  @DisplayName("should not get a user by id")
+  void shouldNotGetById() {
+    when(userServiceMock.find(1L)).thenReturn(Optional.empty());
 
-    @DisplayName("should persist changes to existing user")
-    @Test void shouldPersistChangesToExistingUser() throws Exception {
-        UserDto userDto = new UserDto();
-        userDto.setId(1L);
+    var userRespnse = testRestTemplate.getForEntity(buildFullPath("/user/1"), UserDto.class);
 
-        mockMvc.perform(post("/user/persist")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(objectMapper.writeValueAsBytes(userDto))
-        ).andExpect(status().isOk());
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.NO_CONTENT),
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getBody).as("user").isNull()
+    );
+  }
 
-        verify(userServiceMock).saveOrUpdate(any(UserDto.class));
-    }
+  @Test
+  @DisplayName("should find a user by id")
+  void shouldFindById() {
+    when(userServiceMock.find(1L)).thenReturn(Optional.of(new UserDto()));
 
-    @DisplayName("should create a user")
-    @Test void shouldCreateGuestUser() throws Exception {
-        UserDto userDto = new UserDto();
-        UserDto createdDto = new UserDto();
-        createdDto.setId(1L);
+    var userRespnse = testRestTemplate.getForEntity(buildFullPath("/user/1"), UserDto.class);
 
-        when(userServiceMock.saveOrUpdate(any(UserDto.class))).thenReturn(createdDto);
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.OK),
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getBody).as("user").isNotNull()
+    );
+  }
 
-        mockMvc.perform(post("/user/persist")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(objectMapper.writeValueAsBytes(userDto))
-        )
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id", is(equalTo(1))));
-    }
+  @Test
+  @DisplayName("should modify existing user")
+  void shouldModifyExistingUser() {
+    UserDto userDto = new UserDto();
+    userDto.setId(1L);
 
-    @DisplayName("should find all usernames on active users")
-    @Test void shouldFindAllUsernames() throws Exception {
-        when(userServiceMock.findUsernamesOnActiveUsers())
-                .thenReturn(List.of("bart", "lisa"));
+    when(userServiceMock.saveOrUpdate(any(UserDto.class))).thenReturn(userDto);
 
-        mockMvc.perform(get("/user/all/usernames")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-        ).andExpect(status().isOk())
-                .andExpect(jsonPath("[0]", is(equalTo("bart"))))
-                .andExpect(jsonPath("[1]", is(equalTo("lisa"))));
-    }
+    var userRespnse = testRestTemplate.exchange(buildFullPath("/user/1"), HttpMethod.PUT, new HttpEntity<>(userDto), UserDto.class);
+
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.ACCEPTED),
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getBody).as("user").isNotNull(),
+        () -> assertThat(userRespnse.getBody()).extracting(UserDto::getId).as("user id").isEqualTo(1L),
+        () -> verify(userServiceMock).saveOrUpdate(any(UserDto.class))
+    );
+  }
+
+  @Test
+  @DisplayName("should create a user")
+  void shouldCreateGuestUser() {
+    UserDto userDto = new UserDto();
+    UserDto createdDto = new UserDto();
+    createdDto.setId(1L);
+
+    when(userServiceMock.saveOrUpdate(any(UserDto.class))).thenReturn(createdDto);
+
+    var userRespnse = testRestTemplate.exchange(buildFullPath("/user"), HttpMethod.POST, new HttpEntity<>(userDto), UserDto.class);
+
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.CREATED),
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getBody).as("user").isNotNull(),
+        () -> assertThat(userRespnse.getBody()).extracting(UserDto::getId).as("user id").isEqualTo(1L),
+        () -> verify(userServiceMock).saveOrUpdate(any(UserDto.class))
+    );
+  }
+
+  @Test
+  @DisplayName("should find all usernames on active users")
+  void shouldFindAllUsernames() {
+    when(userServiceMock.findUsernamesOnActiveUsers())
+        .thenReturn(List.of("bart", "lisa"));
+
+    var userRespnse = testRestTemplate.exchange(buildFullPath("/user/active/usernames"), HttpMethod.GET, null, responsIslistOfStrings());
+
+    assertAll(
+        () -> assertThat(userRespnse).extracting(ResponseEntity::getStatusCode).as("status").isEqualTo(HttpStatus.OK),
+        () -> assertThat(userRespnse.getBody()).as("user").isEqualTo(List.of("bart", "lisa"))
+    );
+  }
+
+  private String buildFullPath(String url) {
+    return "http://localhost:" + port + contextPath + url;
+  }
+
+  private ParameterizedTypeReference<List<String>> responsIslistOfStrings() {
+    return new ParameterizedTypeReference<>() {
+    };
+  }
+
 }
