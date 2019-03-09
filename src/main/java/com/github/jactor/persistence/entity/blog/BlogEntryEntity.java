12632957a6022abf93353e78362035290319d426
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-03-09 17:05:48 +0100

    removed attributes overrides from BlogEntryEntity

--- src/main/java/com/github/jactor/persistence/entity/blog/BlogEntryEntity.java
+++ src/main/java/com/github/jactor/persistence/entity/blog/BlogEntryEntity.java
@@ -1,15 +1,16 @@
 package com.github.jactor.persistence.entity.blog;
 
+import static java.util.Objects.hash;
+
 import com.github.jactor.persistence.dto.BlogDto;
 import com.github.jactor.persistence.dto.BlogEntryDto;
 import com.github.jactor.persistence.entity.EntryEmbeddable;
 import com.github.jactor.persistence.entity.PersistentEntity;
 import com.github.jactor.persistence.time.Now;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
-
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
 import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -19,129 +20,137 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import static java.util.Objects.hash;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 @Entity
 @Table(name = "T_BLOG_ENTRY")
 public class BlogEntryEntity extends PersistentEntity {
 
-    private @Id Long id;
+  @Id
+  private Long id;
 
-    private @ManyToOne(cascade = CascadeType.ALL) @JoinColumn(name = "BLOG_ID") BlogEntity blog;
-    private @Embedded @AttributeOverrides({
-            @AttributeOverride(name = "creatorName", column = @Column(name = "CREATOR_NAME")),
-            @AttributeOverride(name = "entry", column = @Column(name = "ENTRY"))
-    }) EntryEmbeddable entryEmbeddable = new EntryEmbeddable();
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "BLOG_ID")
+  private BlogEntity blog;
 
-    BlogEntryEntity() {
-        // used by builder
-    }
+  @Embedded
+  @AttributeOverride(name = "creatorName", column = @Column(name = "CREATOR_NAME"))
+  @AttributeOverride(name = "entry", column = @Column(name = "ENTRY"))
+  private EntryEmbeddable entryEmbeddable = new EntryEmbeddable();
 
-    private BlogEntryEntity(BlogEntryEntity blogEntryEntity) {
-        super(blogEntryEntity);
-        blog = blogEntryEntity.copyBlog();
-        entryEmbeddable = blogEntryEntity.copyEntry();
-    }
+  BlogEntryEntity() {
+    // used by builder
+  }
 
-    public BlogEntryEntity(@NotNull BlogEntryDto blogEntryDto) {
-        super(blogEntryDto.fetchPersistentDto());
-        Optional.ofNullable(blogEntryDto.getBlog()).ifPresent(blogDto -> blog = new BlogEntity(blogDto));
-        entryEmbeddable = new EntryEmbeddable(blogEntryDto.getCreatorName(), blogEntryDto.getEntry());
-    }
+  private BlogEntryEntity(BlogEntryEntity blogEntryEntity) {
+    super(blogEntryEntity);
+    blog = blogEntryEntity.copyBlog();
+    entryEmbeddable = blogEntryEntity.copyEntry();
+  }
 
-    private BlogEntity copyBlog() {
-        return blog.copy();
-    }
+  public BlogEntryEntity(@NotNull BlogEntryDto blogEntryDto) {
+    super(blogEntryDto.fetchPersistentDto());
+    Optional.ofNullable(blogEntryDto.getBlog()).ifPresent(blogDto -> blog = new BlogEntity(blogDto));
+    entryEmbeddable = new EntryEmbeddable(blogEntryDto.getCreatorName(), blogEntryDto.getEntry());
+  }
 
-    private EntryEmbeddable copyEntry() {
-        return entryEmbeddable.copy();
-    }
+  private BlogEntity copyBlog() {
+    return blog.copy();
+  }
 
-    public BlogEntryDto asDto() {
-        return asDto(blog.asDto());
-    }
+  private EntryEmbeddable copyEntry() {
+    return entryEmbeddable.copy();
+  }
 
-    private BlogEntryDto asDto(BlogDto blogDto) {
-        BlogEntryDto blogEntryDto = new BlogEntryDto();
-        blogEntryDto.setBlog(blogDto);
-        blogEntryDto.setCreatorName(entryEmbeddable.getCreatorName());
-        blogEntryDto.setEntry(entryEmbeddable.getEntry());
+  public BlogEntryDto asDto() {
+    return asDto(blog.asDto());
+  }
 
-        return blogEntryDto;
-    }
+  private BlogEntryDto asDto(BlogDto blogDto) {
+    BlogEntryDto blogEntryDto = new BlogEntryDto();
+    blogEntryDto.setBlog(blogDto);
+    blogEntryDto.setCreatorName(entryEmbeddable.getCreatorName());
+    blogEntryDto.setEntry(entryEmbeddable.getEntry());
 
-    public void create(String entry) {
-        setCreationTime(Now.asDateTime());
-        entryEmbeddable.setEntry(entry);
-    }
+    return blogEntryDto;
+  }
 
-    public void update(String entry) {
-        setUpdatedTime(Now.asDateTime());
-        entryEmbeddable.setEntry(entry);
-    }
+  public void create(String entry) {
+    setCreationTime(Now.asDateTime());
+    entryEmbeddable.setEntry(entry);
+  }
 
-    public @Override BlogEntryEntity copy() {
-        return new BlogEntryEntity(this);
-    }
+  public void update(String entry) {
+    setUpdatedTime(Now.asDateTime());
+    entryEmbeddable.setEntry(entry);
+  }
 
-    protected @Override Stream<Optional<PersistentEntity>> streamSequencedDependencies() {
-        return streamSequencedDependencies(blog);
-    }
+  @Override
+  public BlogEntryEntity copy() {
+    return new BlogEntryEntity(this);
+  }
 
-    public @Override boolean equals(Object o) {
-        return this == o || o != null && getClass() == o.getClass() && isEqualTo((BlogEntryEntity) o);
-    }
+  @Override
+  protected Stream<Optional<PersistentEntity>> streamSequencedDependencies() {
+    return streamSequencedDependencies(blog);
+  }
 
-    private boolean isEqualTo(BlogEntryEntity o) {
-        return Objects.equals(entryEmbeddable, o.entryEmbeddable) &&
-                Objects.equals(blog, o.getBlog());
-    }
+  @Override
+  public boolean equals(Object o) {
+    return this == o || o != null && getClass() == o.getClass() && isEqualTo((BlogEntryEntity) o);
+  }
 
-    public @Override int hashCode() {
-        return hash(blog, entryEmbeddable);
-    }
+  private boolean isEqualTo(BlogEntryEntity o) {
+    return Objects.equals(entryEmbeddable, o.entryEmbeddable) &&
+        Objects.equals(blog, o.getBlog());
+  }
 
-    public @Override String toString() {
-        return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE)
-                .appendSuper(super.toString())
-                .append(getBlog())
-                .append(entryEmbeddable)
-                .toString();
-    }
+  @Override
+  public int hashCode() {
+    return hash(blog, entryEmbeddable);
+  }
 
-    public @Override Long getId() {
-        return id;
-    }
+  @Override
+  public String toString() {
+    return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE)
+        .appendSuper(super.toString())
+        .append(getBlog())
+        .append(entryEmbeddable)
+        .toString();
+  }
 
-    protected @Override void setId(Long id) {
-        this.id = id;
-    }
+  @Override
+  public Long getId() {
+    return id;
+  }
 
-    public BlogEntity getBlog() {
-        return blog;
-    }
+  @Override
+  protected void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setBlog(BlogEntity blog) {
-        this.blog = blog;
-    }
+  public BlogEntity getBlog() {
+    return blog;
+  }
 
-    public String getCreatorName() {
-        return entryEmbeddable.getCreatorName();
-    }
+  public void setBlog(BlogEntity blog) {
+    this.blog = blog;
+  }
 
-    public void setCreatorName(String creator) {
-        entryEmbeddable.setCreatorName(creator);
-    }
+  public String getCreatorName() {
+    return entryEmbeddable.getCreatorName();
+  }
 
-    public String getEntry() {
-        return entryEmbeddable.getEntry();
-    }
+  public void setCreatorName(String creator) {
+    entryEmbeddable.setCreatorName(creator);
+  }
 
-    public static BlogEntryEntityBuilder aBlogEntry() {
-        return new BlogEntryEntityBuilder();
-    }
+  public String getEntry() {
+    return entryEmbeddable.getEntry();
+  }
+
+  public static BlogEntryEntityBuilder aBlogEntry() {
+    return new BlogEntryEntityBuilder();
+  }
 }
