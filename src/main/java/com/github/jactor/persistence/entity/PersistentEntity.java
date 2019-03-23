377c1b80f50cf6893045e8ec5c8b72125b782ec2
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-03-23 20:31:05 +0100

    removed manual id for GuestBookEntity

--- src/main/java/com/github/jactor/persistence/entity/PersistentEntity.java
+++ src/main/java/com/github/jactor/persistence/entity/PersistentEntity.java
@@ -34,6 +34,7 @@ public interface PersistentEntity<T> extends PersistentData {
         .filter(dependency -> !(dependency instanceof BlogEntryEntity))
         .filter(dependency -> !(dependency instanceof UserEntity))
         .filter(dependency -> !(dependency instanceof BlogEntity))
+        .filter(dependency -> !(dependency instanceof GuestBookEntity))
         .forEach(depencency -> addSequencedId(depencency, sequencer));
 
     return this;
