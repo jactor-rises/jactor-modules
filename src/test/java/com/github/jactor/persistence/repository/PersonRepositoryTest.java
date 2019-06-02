587102a372329e8e2f0bf45a437b05a6a291d3c7
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-06-02 12:04:24 +0200

    UserControllerIntegrationTest needs to be transactional
    - annotations not needed on PersonRepositoryTest

--- src/test/java/com/github/jactor/persistence/repository/PersonRepositoryTest.java
+++ src/test/java/com/github/jactor/persistence/repository/PersonRepositoryTest.java
@@ -17,12 +17,9 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {JactorPersistence.class})
 @Transactional
 @DisplayName("A PersonRepository")
