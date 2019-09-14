f5d78692c3a5252bb252685d384905e4b91c8b51
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2019-09-14 17:46:25 +0200

    added swagger documentation and changed behaviour of updating user

--- src/main/java/com/github/jactor/persistence/controller/UserController.java
+++ src/main/java/com/github/jactor/persistence/controller/UserController.java
@@ -4,6 +4,9 @@ import com.github.jactor.persistence.command.CreateUserCommand;
 import com.github.jactor.persistence.command.CreateUserCommandResponse;
 import com.github.jactor.persistence.dto.UserDto;
 import com.github.jactor.persistence.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,34 +32,51 @@ public class UserController {
     this.userServicey = userServicey;
   }
 
+  @ApiOperation("Find a user by its username")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "User found"),
+      @ApiResponse(code = 204, message = "No user with username")
+  })
   @GetMapping("/name/{username}")
   public ResponseEntity<UserDto> find(@PathVariable("username") String username) {
     return userServicey.find(username)
         .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
-        .orElseGet(() -> ResponseEntity.noContent().build());
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
   }
 
+  @ApiOperation("Get a user by its id")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "User got"),
+      @ApiResponse(code = 404, message = "Did not find user with id")
+  })
   @GetMapping("/id/{id}")
   public ResponseEntity<UserDto> get(@PathVariable("id") Long id) {
     return userServicey.find(id)
         .map(userDto -> new ResponseEntity<>(userDto, HttpStatus.OK))
-        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
+        .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
   }
 
+  @ApiOperation("Create a user")
+  @ApiResponses(value = {
+      @ApiResponse(code = 201, message = "User created")
+  })
   @PostMapping("/create")
   public ResponseEntity<CreateUserCommandResponse> post(@RequestBody CreateUserCommand createUserCommand) {
-    Long primaryKey = userServicey.create(createUserCommand);
+    var primaryKey = userServicey.create(createUserCommand);
 
     return new ResponseEntity<>(new CreateUserCommandResponse(primaryKey), HttpStatus.CREATED);
   }
 
+  @ApiOperation("Update a user by its id")
+  @ApiResponses(value = {
+      @ApiResponse(code = 202, message = "User updated"),
+      @ApiResponse(code = 400, message = "Did not find user with id")
+  })
   @PutMapping("/id/{userId}")
   public ResponseEntity<UserDto> put(@RequestBody UserDto userDto, @PathVariable Long userId) {
-    if (userDto.getId() == null || !userDto.getId().equals(userId)) {
-      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
+    userDto.setId(userId);
 
-    UserDto saved = userServicey.saveOrUpdate(userDto);
+    var saved = userServicey.saveOrUpdate(userDto);
 
     return new ResponseEntity<>(saved, HttpStatus.ACCEPTED);
   }
