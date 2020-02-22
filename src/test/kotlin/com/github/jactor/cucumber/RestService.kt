ba5af9dd87a8abce571462bf91d22896caeb3f62
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2020-02-22 12:54:40 +0100

    test av opprett bruker

--- src/test/kotlin/com/github/jactor/cucumber/RestService.kt
+++ src/test/kotlin/com/github/jactor/cucumber/RestService.kt
@@ -1,12 +1,18 @@
 package com.github.jactor.cucumber
 
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
 data class RestService(val baseUrl: String, var url: String = "") {
-    private lateinit var responseEntity: ResponseEntity<String>
+    private var httpClientErrorException: HttpClientErrorException? = null
+    private var responseEntity: ResponseEntity<String>? = null
     private var restTemplate: RestTemplate? = null
 
     fun exchangeGet() {
@@ -14,18 +20,42 @@ data class RestService(val baseUrl: String, var url: String = "") {
     }
 
     fun exchangeGet(navn: String?, parameter: String?) {
-        if (restTemplate == null) {
-            restTemplate = RestTemplate()
-        }
-
-        val fullUrl =
-                if (navn != null) UriComponentsBuilder.fromHttpUrl(baseUrl + url)
-                        .queryParam(navn, parameter).build().toUriString()
-                else baseUrl + url
+        initRestTemplate()
+        val fullUrl = initUrl(navn, parameter)
 
         responseEntity = restTemplate!!.exchange(fullUrl, HttpMethod.GET, null, String::class.java)
     }
 
-    fun hentStatusKode() = responseEntity.statusCode
-    fun hentResponse() = responseEntity.body
+    fun exchangePost(json: String) {
+        initRestTemplate()
+        val fullUrl = initUrl()
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+
+        try {
+            responseEntity = restTemplate!!.exchange(fullUrl, HttpMethod.POST, HttpEntity(json, headers), String::class.java)
+        } catch (e: HttpClientErrorException) {
+            httpClientErrorException = e
+        }
+    }
+
+    private fun initUrl() = initUrl(null, null)
+    private fun initUrl(navn: String?, parameter: String?) = if (navn != null) initUrlWithBuilder(navn, parameter) else baseUrl + url
+    private fun initUrlWithBuilder(navn: String, parameter: String?) = UriComponentsBuilder.fromHttpUrl(baseUrl + url).queryParam(navn, parameter)
+            .build().toUriString()
+
+    private fun initRestTemplate() {
+        if (restTemplate == null) {
+            restTemplate = RestTemplate()
+        }
+    }
+
+    fun hentStatusKode(): HttpStatus {
+        if (responseEntity != null) return responseEntity!!.statusCode
+        if (httpClientErrorException != null) return httpClientErrorException!!.statusCode
+
+        return HttpStatus.I_AM_A_TEAPOT
+    }
+
+    fun hentResponse() = responseEntity?.body
 }
