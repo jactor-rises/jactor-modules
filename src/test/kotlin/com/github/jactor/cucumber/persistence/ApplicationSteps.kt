9cf5f058b890316d9dd336fba69c169d6f35fe3d
Tor Egil Jacobsen <jactor-rises@users.noreply.github.com>
2020-12-29 15:13:24 +0100

    cucumber.java -> cucumber.java8

--- src/test/kotlin/com/github/jactor/cucumber/persistence/ApplicationSteps.kt
+++ src/test/kotlin/com/github/jactor/cucumber/persistence/ApplicationSteps.kt
@@ -1,33 +1,30 @@
 package com.github.jactor.cucumber.persistence
 
-import io.cucumber.java.no.Gitt
-import io.cucumber.java.no.Når
-import io.cucumber.java.no.Så
+import io.cucumber.java8.No
 import org.assertj.core.api.Assertions.assertThat
 
-class ApplicationSteps {
+class ApplicationSteps : No {
     companion object {
         private lateinit var application: Application
         private var status: Int? = null
     }
 
-    @Gitt("http url {string}")
-    fun `http url`(url: String) {
-        application = Application(url)
-    }
+    init {
 
-    @Gitt("kompletterende url {string}")
-    fun `kompletterende url`(url: String?) {
-        application.completeUrl = url
-    }
+        Gitt("http url {string}") { url: String ->
+            application = Application(url)
+        }
 
-    @Når("jeg ber om en response kode på en get request")
-    fun `jeg ber om en response kode paa en get request`() {
-        status = application.hentResponseStatusForGet()
-    }
+        Gitt("kompletterende url {string}") { url: String ->
+            application.completeUrl = url
+        }
 
-    @Så("skal statuskoden være {string}")
-    fun `skal statuskoden vaere`(httpCode: String) {
-        assertThat(status).isEqualTo(httpCode.toInt())
+        Når("jeg ber om en response kode på en get request") {
+            status = application.hentResponseStatusForGet()
+        }
+
+        Så("skal statuskoden være {string}") { httpCode: String ->
+            assertThat(status).isEqualTo(httpCode.toInt())
+        }
     }
 }
