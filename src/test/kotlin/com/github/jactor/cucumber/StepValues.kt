bea597118ea32b485bc23e8d7174debbd5e7c3ba
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-12-29 15:13:24 +0100

    cucumber.java -> cucumber.java8

---
+++ src/test/kotlin/com/github/jactor/cucumber/StepValues.kt
@@ -0,0 +1,8 @@
+package com.github.jactor.cucumber
+
+class StepValues {
+    companion object {
+        lateinit var restService: RestService
+        lateinit var uniqueKey: String
+    }
+}
--- src/test/kotlin/com/github/jactor/cucumber/persistence/UserSteps.kt
+++ src/test/kotlin/com/github/jactor/cucumber/persistence/UserSteps.kt
@@ -1,41 +1,35 @@
 package com.github.jactor.cucumber.persistence
 
-import com.github.jactor.cucumber.RestServiceSteps
-import io.cucumber.java.no.Når
-import io.cucumber.java.no.Og
+import com.github.jactor.cucumber.RestService
+import com.github.jactor.cucumber.StepValues.Companion.restService
+import com.github.jactor.cucumber.StepValues.Companion.uniqueKey
+import io.cucumber.java8.No
 import org.assertj.core.api.Assertions.assertThat
 
+class UserSteps : No {
+    init {
+        Når("en post gjøres for unik nøkkel {string} med body:") { keyToBeUnique: String, body: String ->
+            uniqueKey = "$keyToBeUnique.${java.lang.Long.toHexString(System.currentTimeMillis())}"
+            val jsonBody = body.replace(keyToBeUnique, uniqueKey)
+            restService.exchangePost(jsonBody)
+        }
 
-class UserSteps {
-    companion object {
-        private lateinit var uniqueKey: String
-    }
+        Og("gitt nøkkel {string} og url til resttjeneste: {string}") { key: String, url: String ->
+            assertThat(uniqueKey).`as`("ujent \"unik\" nøkkel for skal brukes i url...").startsWith(key)
+            val urlWithUniqueKey = url.replace(key, uniqueKey)
+            restService = RestService(urlWithUniqueKey)
+        }
 
-    @Når("en post gjøres for unik nøkkel {string} med body:")
-    fun `en post gjores for unik nokkel med body`(keyToBeUnique: String, body: String) {
-        uniqueKey = "$keyToBeUnique.${java.lang.Long.toHexString(System.currentTimeMillis())}"
-        val jsonBody = body.replace(keyToBeUnique, uniqueKey)
-        RestServiceSteps().`en post gjores med body`(jsonBody)
-    }
+        Og("url til resttjeneste med den unike nøkkelen: {string}") { key: String, url: String ->
+            assertThat(uniqueKey).`as`("ujent \"unik\" nøkkel for skal brukes i url...").startsWith(key)
+            val urlWithUniqueKey = url.replace(key, uniqueKey)
+            restService = RestService(urlWithUniqueKey)
+        }
 
-    @Og("gitt nøkkel {string} og url til resttjeneste: {string}")
-    fun `gitt nokkel og url til resttjeneste`(key: String, url: String) {
-        assertThat(uniqueKey).`as`("ujent \"unik\" nøkkel for skal brukes i url...").startsWith(key)
-        val urlWithUniqueKey = url.replace(key, uniqueKey)
-        RestServiceSteps().`url til resttjeneste`(urlWithUniqueKey)
-    }
-
-    @Og("url til resttjeneste med den unike nøkkelen: {string}")
-    fun `url til resttjeneste med den unike nokkelen`(key: String, url: String) {
-        assertThat(uniqueKey).`as`("ujent \"unik\" nøkkel for skal brukes i url...").startsWith(key)
-        val urlWithUniqueKey = url.replace(key, uniqueKey)
-        RestServiceSteps().`url til resttjeneste`(urlWithUniqueKey)
-    }
-
-    @Når("en post gjøres for unik nøkkel {string}, men den unike nøkkelen gjenbrukes på body:")
-    fun `en post gjores og den unike nokkelen gjenbrukes pa body`(key: String, body: String) {
-        assertThat(uniqueKey).`as`("ujent \"unik\" nøkkel som skal gjenbrukes i json body...").startsWith(key)
-        val jsonBody = body.replace(key, uniqueKey)
-        RestServiceSteps().`en post gjores med body`(jsonBody)
+        Når("en post gjøres for unik nøkkel {string}, men den unike nøkkelen gjenbrukes på body:") { key: String, body: String ->
+            assertThat(uniqueKey).`as`("ujent \"unik\" nøkkel som skal gjenbrukes i json body...").startsWith(key)
+            val jsonBody = body.replace(key, uniqueKey)
+            restService.exchangePost(jsonBody)
+        }
     }
 }
