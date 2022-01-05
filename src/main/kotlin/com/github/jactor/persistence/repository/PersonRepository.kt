f7032c7cdfb9a1d76511dcb44f30b631db240dde
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2022-01-05 15:08:28 +0100

    PersonRepositoryTest is now kotlin

--- src/main/kotlin/com/github/jactor/persistence/repository/PersonRepository.kt
+++ src/main/kotlin/com/github/jactor/persistence/repository/PersonRepository.kt
@@ -3,6 +3,6 @@ package com.github.jactor.persistence.repository
 import com.github.jactor.persistence.entity.PersonEntity
 import org.springframework.data.repository.CrudRepository
 
-interface PersonRepository : CrudRepository<PersonEntity?, Long?> {
-    fun findBySurname(surname: String?): List<PersonEntity?>
+interface PersonRepository : CrudRepository<PersonEntity, Long?> {
+    fun findBySurname(surname: String?): List<PersonEntity>
 }
