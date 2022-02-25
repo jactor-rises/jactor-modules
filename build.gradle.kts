297145e76f637249c83d26afd3bf8df2b5aa2539
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-02-25 08:23:31 +0100

    spring-boot 2.6.3 is moved to root build.gradle.kts
    - org.springframework.boot
    - io.spring.dependency-management
    - org.jetbrains.kotlin.plugin.spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
 plugins {
     id("com.github.ben-manes.versions") version "0.42.0" apply false
+    id("org.springframework.boot") version "2.6.3" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+
+    kotlin("plugin.spring") version "1.6.20-M1" apply false
 }
 
 subprojects {
     apply(plugin = "jactor-modules-kotlin-conventions")
     apply(plugin = "com.github.ben-manes.versions")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 }
