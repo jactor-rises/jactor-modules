b2f8981ddad680109ad2a1c6b1fe4ed11d963472
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2022-08-20 15:58:49 +0200

    bump springdocopenapi

--- buildSrc/src/main/kotlin/library.kt
+++ buildSrc/src/main/kotlin/library.kt
@@ -13,7 +13,7 @@ object JactorModules {
         const val flyway = "8.5.13"
         const val h2 = "2.1.214"
         const val mockk = "1.12.4"
-        const val springdocOpenApi = "1.6.9"
+        const val springdocOpenApi = "1.6.10"
         const val springmockk = "3.1.1"
         const val webjarsBootstrap = "5.1.3"
         const val webjarsJquery = "3.6.0"
