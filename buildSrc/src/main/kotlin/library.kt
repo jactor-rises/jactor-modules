dafff7ede50c833fcadf9620325c4a77ec712eb8
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2023-09-27 15:25:50 +0200

    bump mockk -> 1.13.8

--- buildSrc/src/main/kotlin/library.kt
+++ buildSrc/src/main/kotlin/library.kt
@@ -11,7 +11,7 @@ object Versions {
     // dependencies
     const val v9_22_0 = "9.22.0"
     const val v2_2_222 = "2.2.222"
-    const val v1_13_7 = "1.13.7"
+    const val v1_13_8 = "1.13.8"
     const val v1_7_0 = "1.7.0"
     const val v4_0_2 = "4.0.2"
     const val v5_2_3 = "5.2.3"
