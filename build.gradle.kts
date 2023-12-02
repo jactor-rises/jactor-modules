09ae49282cc34331bf450fa0da43fa07e6948eb4
Tor Egil Jacobsen <tor.egil.jacobsen@gmail>
2023-12-02 17:27:02 +0100

    remove com.github.ben-manes from Versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 
 plugins {
-    id("com.github.ben-manes.versions") version Versions.V0_46_0 apply false
+    id("com.github.ben-manes.versions") version "0.46.0" apply false
     id("io.spring.dependency-management") version Versions.V1_1_0 apply false
 
     kotlin("plugin.spring") version Versions.V1_9_10 apply false
