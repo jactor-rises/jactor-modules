c3c1b67fe69943db3dce1d1c45a6907c67ce2e5b
Tor Egil Jacobsen <tor.egil.jacobsen@gmail>
2023-12-02 18:17:16 +0100

    bump jquery -> 3.7.1

--- buildSrc/src/main/kotlin/library.kt
+++ buildSrc/src/main/kotlin/library.kt
@@ -3,6 +3,5 @@ object Versions {
     // kotlin language version and kotlin-gradle plugin is an implementation defined in buildSrc/build.gradle.kts
 
     // dependencies
-    const val V3_6_4 = "3.6.4"
     const val V4_0_2 = "4.0.2"
 }
