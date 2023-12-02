9ed994dfcc5703280910a1e4e20758a5959dfb4a
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2023-12-02 18:06:12 +0100

    bump spring-boot -> 3.2.0

--- buildSrc/src/main/kotlin/library.kt
+++ buildSrc/src/main/kotlin/library.kt
@@ -2,9 +2,6 @@ object Versions {
     // PS!
     // kotlin language version and kotlin-gradle plugin is an implementation defined in buildSrc/build.gradle.kts
 
-    // plugins
-    const val V3_1_4 = "3.1.4"
-
     // dependencies
     const val V1_13_8 = "1.13.8"
     const val V3_6_4 = "3.6.4"
