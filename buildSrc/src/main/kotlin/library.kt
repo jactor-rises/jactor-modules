1f5660123ff315065d9f7bf9991daab91c973ff7
Tor Egil Jacobsen <tor.egil.jacobsen@gmail>
2023-12-02 18:01:37 +0100

    remove h2databse from Versions

--- buildSrc/src/main/kotlin/library.kt
+++ buildSrc/src/main/kotlin/library.kt
@@ -7,7 +7,6 @@ object Versions {
 
     // dependencies
     const val V1_13_8 = "1.13.8"
-    const val V2_2_224 = "2.2.224"
     const val V3_6_4 = "3.6.4"
     const val V4_0_2 = "4.0.2"
     const val V5_2_3 = "5.2.3"
