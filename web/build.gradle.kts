78a4abfdc668d89f3976befb89ecfc6b73dc4e27
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2024-04-15 11:19:17 +0200

    bump spring-boot -> 3.2.4

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -3,7 +3,7 @@ version = "1.0.x-SNAPSHOT"
 description = "jactor-web"
 
 plugins {
-    id("org.springframework.boot") version "3.2.1"
+    id("org.springframework.boot") version "3.2.4"
 }
 
 dependencies {
