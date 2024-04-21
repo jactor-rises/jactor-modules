39b6264a9713a6b39a46692ece34e4a948f05caf
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2024-04-21 10:35:35 +0200

    bump springdoc-openapi -> 7.17.0

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -24,5 +24,5 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // swagger
-    implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
+    implementation("org.springdoc:springdoc-openapi-ui:1.8.0")
 }
