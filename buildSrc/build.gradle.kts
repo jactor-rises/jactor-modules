9be96827c9d939de745b6d12a87e46794234f17c
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2024-11-01 15:45:45 +0100

    bump kotlin -> 2.0.21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -18,5 +18,5 @@ tasks.compileKotlin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
 }
