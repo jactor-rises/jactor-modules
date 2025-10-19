e6bd28f75e06a09a9189bff6b54eb93299e58eca
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2025-10-19 20:42:23 +0200

    no lazy loading of relations, ie: deleted DaoRelation and DaoRelations
    also
    - missing *Entity -> *Dao

--- web/src/main/kotlin/com/github/jactor/web/model/models.kt
+++ web/src/main/kotlin/com/github/jactor/web/model/models.kt
@@ -1,9 +1,9 @@
 package com.github.jactor.web.model
 
 import com.github.jactor.shared.api.AddressDto
-import com.github.jactor.shared.api.PersonDto
 import com.github.jactor.shared.api.UserDto
 import com.github.jactor.web.Technology
+import java.util.UUID
 
 @JvmRecord
 data class HomePageModel(val technologies: List<Technology> = ArrayList())
@@ -11,44 +11,14 @@ data class HomePageModel(val technologies: List<Technology> = ArrayList())
 @JvmRecord
 data class UserModel(
     private val user: UserDto,
-    private val person: PersonDto = user.person ?: PersonDto(),
-    private val address: AddressDto = person.address ?: AddressDto()
+    private val personId: UUID = user.personId ?: UUID.randomUUID(),
 ) {
-    constructor(user: UserDto) : this(user = user, person = user.person ?: PersonDto())
+    val username: String?
+        get() = user.username
 
-    fun fetchAddress(): List<String> {
-        val allFields = listOf(
-            address.addressLine1,
-            address.addressLine2,
-            address.addressLine3,
-            address.zipCode,
-            address.city
-        )
-
-        val fieldsNotNull = mutableListOf<String>()
-
-        allFields.forEach {
-            if (it != null) {
-                fieldsNotNull.add(it)
-            }
-        }
-
-        return fieldsNotNull
-    }
-
-    fun fetchFullName(): String {
-        val surname = person.surname
-        val firstName = person.firstName ?: ""
-        val fullName = "$firstName $surname".trim()
-
-        return fullName.ifEmpty { throw IllegalStateException("Unable to determine name of person") }
-    }
+    constructor(user: UserDto) : this(user = user, personId = user.personId ?: UUID.randomUUID())
 
     fun fetchUsername(): String? {
         return user.username
-    }
-
-    fun fetchDescriptionCode(): String? {
-        return person.description
     }
 }
