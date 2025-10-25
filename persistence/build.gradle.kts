0daffdc8fe2f1280bd0b78c6dd44fa07fe763e34
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2025-10-25 14:03:47 +0200

    #104: build system is gradle 9.1.0
    jactor-modules-kotlin-conventions.gradle.kts is replaced by jactor-modules-spring-application.gradle.kts and jactor-modules-spring-library.gradle.kts
    
    todo: javatime in exposed

--- persistence/build.gradle.kts
+++ persistence/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("jactor-modules-spring-application")
+}
+
 group = "com.github.jactor-rises"
 version = "2.0.x-SNAPSHOT"
 description = "jactor::persistence"
@@ -6,10 +10,6 @@ val exposedVersion: String by project
 val flywayVersion: String by project
 val h2DatabaseVersion: String by project
 
-plugins {
-    id("org.springframework.boot") version "3.5.5"
-}
-
 dependencies {
     // spring-boot
     implementation("org.springframework.boot:spring-boot-starter-actuator")
@@ -17,16 +17,26 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
 
-    // internal project dependency
-    implementation(project(":shared"))
+    // kotlin
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // jetbrains-exposed
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:${exposedVersion}")
     implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+
+    // internal project dependency
+    implementation(project(":shared"))
 
     // runtime dependencies
     runtimeOnly("org.flywaydb:flyway-core:$flywayVersion")
     runtimeOnly("com.h2database:h2:$h2DatabaseVersion")
+
+    // test dependencies
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testRuntimeOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
 tasks.test {
